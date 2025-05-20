08d166c027cd56094f438a4bca2731ddecc1b0f6
Wassim <wassim.besghayer@gmail.com>
2025-05-20 23:21:04 +0100

    v.1

--- server/models/user.js
+++ server/models/user.js
@@ -5,11 +5,12 @@ const data_model=mongoose.Schema;
 
 // Describing what a user should contain as a schema
 const user_schema = new data_model({
-    name: String,
-    surname: String,
-    birthday: Number,
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    // birthday: {type: Date, required: true, trim: true},
+    birthday: {type: String, required: true},
     phone: {type: Number, unique: true},
-    email: {type: String, unique: true},
+    email: {type: String, unique: true, required: true},
     bank_card: {type: String, unique: true},
     isAdmin: {type: Boolean, default: false}
 })
