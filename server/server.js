a360436c34eaaf944c5a257d30f6ca0013b8f4c1
Wassim <wassim.besghayer@gmail.com>
2025-05-20 22:40:50 +0100

    v.1

--- server/server.js
+++ server/server.js
@@ -11,5 +11,16 @@ require("dotenv").config();
 const DB_connecting=require("./config/connect_DB");
 DB_connecting();
 
+// enabling express to read .json files
+app.use(express.json());
+
+// activating CORS
+app.use(cors());
+
+// routing
+app.use("/user", require("./routes/user"));
+app.use("/product", require("./routes/product"));
+app.use("/order", require("./routes/order"));
+
 // routing: assigning port + catching errors
 app.listen(process.env.port, (err)=>err?console.log(err):console.log("server is running well !"))
