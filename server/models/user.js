d19226b1f4ea83cbd4c2703f8ded47e157b3c951
Wassim <wassim.besghayer@gmail.com>
2025-05-23 21:36:54 +0100

    v.2

--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,34 @@
-// defining 'mongoose'
-const mongoose=require("mongoose");
-// calling 'Schema' from Mongoose (it is a function)
-const data_model=mongoose.Schema;
 
-// Describing what a user should contain as a schema
-const user_schema = new data_model({
-    name: {type: String, required: true},
-    surname: {type: String, required: true},
-    // birthday: {type: Date, required: true, trim: true},
-    birthday: {type: String, required: true},
-    phone: {type: Number, unique: true},
-    email: {type: String, unique: true, required: true},
-    bank_card: {type: String, unique: true},
-    isAdmin: {type: Boolean, default: false}
-})
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const UserSchema = new schema({
+  
+  name: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
 
-// model creation
-const User=mongoose.model('User', user_schema)
-module.exports=User
+module.exports = mongoose.model("user", UserSchema);
+
+
+// // defining 'mongoose'
+// const mongoose=require("mongoose");
+// // calling 'Schema' from Mongoose (it is a function)
+// const data_model=mongoose.Schema;
+
+// // Describing what a user should contain as a schema
+// const user_schema = new data_model({
+//     name: {type: String, required: true},
+//     surname: {type: String, required: true},
+//     // birthday: {type: Date, required: true, trim: true},
+//     birthday: {type: String, required: true},
+//     phone: {type: Number, unique: true},
+//     email: {type: String, unique: true, required: true},
+//     bank_card: {type: String, unique: true},
+//     isAdmin: {type: Boolean, default: false}
+// })
+
+// // model creation
+// const User=mongoose.model('User', user_schema)
+// module.exports=User
