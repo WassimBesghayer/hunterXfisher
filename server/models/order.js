5078730b3142fb0c39a79fa0874a9048c0c62874
Wassim <wassim.besghayer@gmail.com>
2025-05-27 22:33:56 +0100

    v.2.1

--- server/models/order.js
+++ server/models/order.js
@@ -6,8 +6,10 @@ const data_model = mongoose.Schema;
 // describing the order Schema
 const order_schema = new data_model({
     order_number: {type: String, unique: true},
+    buyer:String,
+    productname:String,
+    productimage:String,
     amount: Number,
-    items: String,
     qty: Number,
     purchase_date: {type: Date, default: Date.now}
 })
