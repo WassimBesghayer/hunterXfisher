fd8c10ce7d905735d2bf558bb46f47566e9754f7
Wassim <wassim.besghayer@gmail.com>
2025-05-27 22:42:22 +0100

    init

---
+++ client/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBarr from './NavBarr'
+import Footer from './Footer'
+
+function About() {
+  return (
+    <div>
+        <NavBarr/>
+        hello dear customer !
+        <Footer/>
+    </div>
+  )
+}
+
+export default About
