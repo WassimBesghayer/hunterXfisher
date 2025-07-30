624975ede144450bc67bd6979246876f41515f4d
Wassim <wassim.besghayer@gmail.com>
2025-07-30 23:55:25 +0100

    v.2.2

--- server/models/user.js
+++ server/models/user.js
@@ -1,37 +1,27 @@
+// defining 'mongoose'
 const mongoose = require("mongoose");
 
+// importing 'mongoose' to create a schema for the user model
+// 'mongoose' is a library that provides a straightforward way to model data in MongoDB
+// calling 'Schema' from Mongoose (it is a function)
 const schema = mongoose.Schema;
 
+// Describing what a user should contain as a schema
 const UserSchema = new schema({
+  picture: { type: String, default: "https://www.w3schools.com/howto/img_avatar.png" },
   name: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  phone: {type: String, required: true},
+  bank_card: {type: String, required: true},
+  birthday: {type: String, required: true},
+  address: {type: String},
+  category: { type: String, default: "user" },
 });
 
-// Avoid OverwriteModelError
+
+// 'mongoose.models.user' is used to avoid overwriting the model if it already exists
+// This is useful in development environments where the model might be redefined multiple times
+// model creation
 module.exports = mongoose.models.user || mongoose.model("user", UserSchema);
-
-
-
-
-// // defining 'mongoose'
-// const mongoose=require("mongoose");
-// // calling 'Schema' from Mongoose (it is a function)
-// const data_model=mongoose.Schema;
-
-// // Describing what a user should contain as a schema
-// const user_schema = new data_model({
-//     name: {type: String, required: true},
-//     surname: {type: String, required: true},
-//     // birthday: {type: Date, required: true, trim: true},
-//     birthday: {type: String, required: true},
-//     phone: {type: Number, unique: true},
-//     email: {type: String, unique: true, required: true},
-//     bank_card: {type: String, unique: true},
-//     isAdmin: {type: Boolean, default: false}
-// })
-
-// // model creation
-// const User=mongoose.model('User', user_schema)
-// module.exports=User
