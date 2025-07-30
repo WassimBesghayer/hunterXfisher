0c0901d810f998344f85d12065d3a11c5aaa18a5
Wassim <wassim.besghayer@gmail.com>
2025-07-30 17:16:08 +0100

    Add Dashboard route and refactor user data fetching in App component

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,29 @@ import Shop from './components/Shop';
 import { getproduct } from './redux/productSlice';
 import { useEffect, useState } from 'react';
 import { useDispatch } from "react-redux";
-
 import Profil from "./components/Profil";
 import Register from "./components/Register";
-
 import PrivateRoute from "./Routes/PrivateRouter";
 import Login from "./components/Login";
-import { userCurrent } from './redux/userSlice';
+import { getuser, userCurrent } from './redux/userSlice';
 import Hunting from './components/Hunting';
 import Camping from './components/Camping';
 import Fishing from './components/Fishing';
 import Basket from './components/Basket';
 import { getorder } from './redux/orderSlice';
+import Dashboard from './components/Dashboard/Dashboard';
+
 
 
 function App() {
-const dispatch=useDispatch();
-const [ping, setping] = useState(false)
-  useEffect(() => {
-     dispatch(getproduct());
+  const dispatch=useDispatch();
+  const [ping, setping] = useState(false)
+    useEffect(() => {
+      dispatch(getproduct());
       dispatch(userCurrent());
       dispatch(getorder());
-
-  }, [ping])
+      dispatch(getuser());
+    }, [ping])
 
   return (
     <div className="App">
@@ -42,8 +42,11 @@ const [ping, setping] = useState(false)
           <Route path="/camping" element={<Camping  ping={ping} setping={setping} />} />
           <Route path="/basket" element={<Basket  ping={ping} setping={setping}/>} />
           <Route path="/fishing" element={<Fishing  ping={ping} setping={setping} />} />
-
-        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard  ping={ping} setping={setping} />} />
+          {/* <Route path="/tableau-de-bord" element={<Tableau_de_bord ping={ping} setping={setping} /> */}
+          {/* <Route path="/monitoring" element={<Monitoring ping={ping} setping={setping} /> */}
+          
+          <Route element={<PrivateRoute />}>
           <Route path="/profil" element={<Profil ping={ping} setping={setping} />} />
         </Route>
 
